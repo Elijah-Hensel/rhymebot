86cd1067285ac1c1cf39328b7b8fb6b1ccc0a56c
Elijah Hensel <elijah.witth@gmail.com>
2021-09-10 12:17:53 -0400

    rhymeBot initial commit with meaningful functionality

---
+++ commands/youtube.js
@@ -0,0 +1,69 @@
+const axios = require("axios");
+const { MessageEmbed } = require("discord.js");
+
+let rhymes = [];
+
+async function getRhyme(args) {
+  console.log(rhymes, "PRE FETCH");
+
+  try {
+    const { data } = await axios.get(
+      `https://api.datamuse.com/words?rel_rhy=${args}`
+    );
+
+    return data;
+  } catch (err) {
+    console.error("Could not fetch rhymes");
+    throw err;
+  }
+}
+
+async function mapRhymes(args) {
+  if (rhymes.length > 1) {
+    rhymes = [];
+  }
+  try {
+    const data = await getRhyme(args);
+    if (!data) {
+      return;
+    }
+    data.map((word) => {
+      if (rhymes.length < 20) {
+        rhymes.push(word.word);
+      }
+    });
+  } catch (err) {
+    console.error("could not map rhymes");
+    throw err;
+  }
+}
+
+module.exports = {
+  name: "!rhyme",
+  description: "simple rhyme generator",
+  async execute(msg, args) {
+    try {
+      await mapRhymes(args);
+
+      console.log(rhymes, "POST FETCH");
+
+      if (rhymes.length < 1) {
+        msg.reply(`Could not retrieve words that rhyme with ${args}`);
+        return;
+      }
+
+      const rhymeEmbed = new MessageEmbed()
+        .setColor("#FF1493")
+        .setTitle(
+          `
+              HERE ARE SOME RHYMES FOR YOU
+              `
+        )
+        .addFields({ name: "Rhymes", value: `${rhymes.join(`\n`)}` });
+      msg.reply({ embeds: [rhymeEmbed] });
+    } catch (err) {
+      console.error("Could not return mapped rhymes");
+      throw err;
+    }
+  },
+};
--- index.js
+++ index.js
@@ -1,22 +1,29 @@
-require('dotenv').config();
-const Discord = require('discord.js');
-const bot = new Discord.Client();
-bot.commands = new Discord.Collection();
-const botCommands = require('./commands');
+require("dotenv").config();
+const { Client, Intents, Collection } = require("discord.js");
+const bot = new Client({
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    // Intents.FLAGS.GUILD_MEMBERS,
+  ],
+});
+bot.commands = new Collection();
+const botCommands = require("./commands");
+const TOKEN = process.env.TOKEN;
 
-Object.keys(botCommands).map(key => {
+Object.keys(botCommands).map((key) => {
   bot.commands.set(botCommands[key].name, botCommands[key]);
 });
 
-const TOKEN = process.env.TOKEN;
-
 bot.login(TOKEN);
 
-bot.on('ready', () => {
+bot.on("ready", () => {
   console.info(`Logged in as ${bot.user.tag}!`);
+  // var channel = bot.channels.get('832783478269411360');
+  // channel.sendMessage("**Hello! I am the Rhyme-Bottapatamous and my rhymes-schemes are bottomless**\n```To use my magical rhymetastical abilities, insert with ease a word if you'd please \n !rhyme <word>```");
 });
 
-bot.on('message', msg => {
+bot.on("message", (msg) => {
   const args = msg.content.split(/ +/);
   const command = args.shift().toLowerCase();
   console.info(`Called command: ${command}`);
@@ -27,6 +34,6 @@ bot.on('message', msg => {
     bot.commands.get(command).execute(msg, args);
   } catch (error) {
     console.error(error);
-    msg.reply('there was an error trying to execute that command!');
+    msg.reply("there was an error trying to execute that command!");
   }
 });
